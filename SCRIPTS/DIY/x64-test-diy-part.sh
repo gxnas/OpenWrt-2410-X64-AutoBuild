1354284f38e694e00d12fcc4800a30104886a2e4
gxnas <52808122+gxnas@users.noreply.github.com>
2024-12-14 18:46:33 +0800

    Update x64-test-diy-part.sh

--- SCRIPTS/DIY/x64-test-diy-part.sh
+++ SCRIPTS/DIY/x64-test-diy-part.sh
@@ -14,15 +14,11 @@
 
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-echo 'src-git istore https://github.com/jeessy2/ddns-go;master' >> feeds.conf.default
-echo 'src-git istore https://github.com/danchexiaoyang/luci-app-onliner;main' >> feeds.conf.default
-echo 'src-git istore https://github.com/gdy666/luci-app-lucky;main' >> feeds.conf.default
-echo 'src-git istore https://github.com/asvow/luci-app-tailscale;main' >> feeds.conf.default
-./scripts/feeds update istore
-./scripts/feeds update ddns-go
-./scripts/feeds update luci-app-onliner
-./scripts/feeds update luci-app-lucky
-./scripts/feeds update luci-app-tailscale
+echo 'src-git ddns-go https://github.com/jeessy2/ddns-go;master' >> feeds.conf.default
+echo 'src-git onliner https://github.com/danchexiaoyang/luci-app-onliner;main' >> feeds.conf.default
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky;main' >> feeds.conf.default
+echo 'src-git tailscale https://github.com/asvow/luci-app-tailscale;main' >> feeds.conf.default
+./scripts/feeds update istore ddns-go luci-app-onliner luci-app-tailscale
 ./scripts/feeds install -d y -p istore luci-app-store ddns-go luci-app-onliner luci-app-lucky luci-app-tailscale
 
 echo "开始 DIY 配置……"
