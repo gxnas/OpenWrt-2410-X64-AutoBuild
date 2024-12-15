ddd434036cc3b148ce6f106e9a76d535dcba3a30
gxnas <52808122+gxnas@users.noreply.github.com>
2024-12-15 10:57:39 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -46,7 +46,7 @@ sbwfw876_repo="https://github.com/sbwml/openwrt_helloworld"
 sbw_pkg_repo="https://github.com/sbwml/openwrt_pkgs"
 natmap_repo="https://github.com/blueberry-pie-11/luci-app-natmap"
 xwrt_repo="https://github.com/QiuSimons/openwrt-natflow"
-#gxnas_repo="https://github.com/gxnas/OpenWrt_Build_x64_Packages"
+kenzok8_repo="https://github.com/kenzok8/small-package"
 
 # 开始克隆仓库，并行执行
 clone_repo $openwrt_repo $latest_release openwrt &
@@ -61,7 +61,7 @@ clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $openwrt_add_repo master OpenWrt-Add &
 clone_repo $dockerman_repo master dockerman &
 clone_repo $docker_lib_repo master docker_lib &
-#clone_repo $gxnas_repo main gxnas_packages &
+clone_repo $kenzok8_repo main kenzok8_packages &
 # 等待所有后台任务完成
 wait
 
